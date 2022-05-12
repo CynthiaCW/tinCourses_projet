1f1cd42a673baabc1cf02b18d64483965971f5c2
CynthiaCW <cynthia.colombelwnek@gmail.com>
2022-05-12 10:03:13 +0200

    Controller ARTICLE SHOW | VALIDATE

---
+++ src/Controller/Article/ArticleShowController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Article;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/article')]
+class ArticleShowController extends AbstractController
+{
+
+    #[Route('/{id}', name: 'app_article_show', methods: ['GET'])]
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+}
---
+++ src/Controller/Article/ArticleValidateController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Article;
+
+use App\Entity\Ticket;
+use App\Form\TicketType;
+use App\Repository\TicketRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/article/validate')]
+class ArticleValidateController extends AbstractController
+{
+
+    #[Route('/new', name: 'app_article_validate_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, TicketRepository $ticketRepository): Response
+    {
+        $ticket = new Ticket();
+        $form = $this->createForm(TicketType::class, $ticket);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ticketRepository->add($ticket, true);
+
+            return $this->redirectToRoute('app_article_validate_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('article_validate/new.html.twig', [
+            'ticket' => $ticket,
+            'form' => $form,
+        ]);
+    }
+
+
+
+
+
+
+}
